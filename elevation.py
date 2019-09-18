90396e7ffbf2907c0cf6d1635cbbac3ee31a4edf
notoraptor <stevenbocco@gmail.com>
2019-09-18 15:28:56 -0400

    up

--- elevation.py
+++ elevation.py
@@ -1,30 +1,69 @@
 import argparse
+import urllib.parse
+import urllib.request
+from typing import List, Tuple
+from geopy.distance import geodesic
+from datetime import datetime
 import ujson as json
 from PIL import Image
-from urllib.parse import urlencode
-from urllib.request import urlopen
-from typing import List, Tuple
 
 ELEVATION_BASE_URL = 'https://maps.googleapis.com/maps/api/elevation/json'
 API_KEY = 'AIzaSyAMXLOTP3THijZgZGwmoyXvYMNf_a9jF4Y'
-NB_DIVISIONS = 20
-LEN_SIDE = NB_DIVISIONS + 1
-NB_POINTS = LEN_SIDE * LEN_SIDE
 
-def get_elevation(locations=()):
+class Profile(object):
+    __slots__ = ('seconds', 'microseconds')
+
+    def __init__(self, time_start, time_end):
+        difference = time_end - time_start
+        self.seconds = difference.seconds + difference.days * 24 * 3600
+        self.microseconds = difference.microseconds
+
+    def __str__(self):
+        hours = self.seconds // 3600
+        minutes = (self.seconds - 3600 * hours) // 60
+        seconds = (self.seconds - 3600 * hours - 60 * minutes)
+        pieces = []
+        if hours:
+            pieces.append('%d h' % hours)
+        if minutes:
+            pieces.append('%d min' % minutes)
+        if seconds:
+            pieces.append('%d sec' % seconds)
+        if self.microseconds:
+            pieces.append('%d microsec' % self.microseconds)
+        return '(%s)' % (' '.join(pieces) if pieces else '0 sec')
+
+
+def _get_elevations(locations=()):
     # type: (List[Tuple[float, float]]) -> List
+    if not locations:
+        return []
     parameters = {
         'locations': '|'.join('%s,%s' % (lat, lng) for (lat, lng) in locations),
         'key': API_KEY
     }
-    url = '%s?%s' % (ELEVATION_BASE_URL, urlencode(parameters))
-    print(len(url))
-    response = json.loads(urlopen(url).read().decode())
-    if response['status'] != 'OK':
-        raise RuntimeError(response['status'])
-    if len(response['results']) != len(locations):
+    data = urllib.parse.urlencode(parameters)
+    url = '%s?%s' % (ELEVATION_BASE_URL, data)
+    with urllib.request.urlopen(url) as response:
+        decoded_response = json.loads(response.read().decode())
+    if decoded_response['status'] != 'OK':
+        raise RuntimeError(decoded_response['status'])
+    if len(decoded_response['results']) != len(locations):
         raise RuntimeError('INVALID_RESPONSE_LENGTH')
-    return response['results']
+    return decoded_response['results']
+
+
+def get_elevations(locations=()):
+    # type: (List[Tuple[float, float]]) -> List
+    batch_size = 350
+    output = []
+    cursor = 0
+    nb_locations = len(locations)
+    while cursor < nb_locations:
+        print('Getting', cursor, 'to', cursor + batch_size, 'on', nb_locations)
+        output.extend(_get_elevations(locations[cursor:(cursor + batch_size)]))
+        cursor += batch_size
+    return output
 
 
 def main():
@@ -34,7 +73,11 @@ def main():
     parser.add_argument('se_lat', type=float)
     parser.add_argument('se_lng', type=float)
     parser.add_argument('--output', '-o', type=str, required=True)
+    parser.add_argument('--resolution', '-r', type=float, default=10)
+    parser.add_argument('--compute-only', '-c', action='store_true')
     args = parser.parse_args()
+
+    resolution = args.resolution
     nw_lat = args.nw_lat
     nw_lng = args.nw_lng
     se_lat = args.se_lat
@@ -43,20 +86,38 @@ def main():
     ne_lng = se_lng
     sw_lat = se_lat
     sw_lng = nw_lng
+
+    lat_distance = geodesic((nw_lat, nw_lng), (sw_lat, sw_lng)).meters
+    lng_distance = geodesic((nw_lat, nw_lng), (ne_lat, ne_lng)).meters
+    nb_div_lat = round(lat_distance / resolution)
+    nb_div_lng = round(lng_distance / resolution)
     lat_gap = abs(sw_lat - nw_lat)
     lng_gap = abs(se_lng - sw_lng)
-    lat_step = -(lat_gap / NB_DIVISIONS)
-    lng_step = lng_gap / NB_DIVISIONS
+    lat_step = -(lat_gap / nb_div_lat)
+    lng_step = lng_gap / nb_div_lng
+    width = nb_div_lng + 1
+    height = nb_div_lat + 1
+    nb_points = width * height
+    print('Computing', width, 'x', height, '=', nb_points, 'points')
+    if args.compute_only:
+        return
+
     locations = [(nw_lat, nw_lng)]
-    for i in range(1, NB_POINTS):
+    for i in range(1, nb_points):
         previous_lat, previous_lng = locations[-1]
-        lat = previous_lat + lat_step if i % LEN_SIDE == 0 else previous_lat
-        lng = nw_lng if i % LEN_SIDE == 0 else previous_lng + lng_step
+        lat = previous_lat + lat_step if i % width == 0 else previous_lat
+        lng = nw_lng if i % width == 0 else previous_lng + lng_step
         locations.append((lat, lng))
-    print('Computed', LEN_SIDE, 'x', LEN_SIDE, '=', len(locations), 'points')
+    print('Computed', width, 'x', height, '=', len(locations), 'points')
     print('Latest point:   ', locations[-1])
     print('Latest expected:', (se_lat, se_lng))
-    results = get_elevation(locations)
+
+    print('Getting elevations')
+    time_start = datetime.now()
+    results = get_elevations(locations)
+    time_end = datetime.now()
+    print('Got elevations', Profile(time_start, time_end))
+
     image_data = []
     min_resolution = None
     max_resolution = None
@@ -76,13 +137,15 @@ def main():
             max_elevation = elevation
         elevations.append(elevation)
     if min_elevation == max_elevation:
-        image_data = [0] * NB_POINTS
+        image_data = [0] * nb_points
     else:
         for elevation in elevations:
-            image_data.append(int(255 * (elevation - min_elevation) / (max_elevation - min_elevation)))
-    output_image = Image.new(mode='L', size=(LEN_SIDE, LEN_SIDE), color=0)
+            image_data.append(
+                int(255 * (elevation - min_elevation) / (max_elevation - min_elevation)))
+    output_image = Image.new(mode='L', size=(width, height), color=0)
     output_image.putdata(image_data)
-    output_image.save(args.name)
+    output_image.save(args.output)
+
 
 if __name__ == '__main__':
     main()
